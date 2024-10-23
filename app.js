ed9aff8e4a8c7502cd623a8b6a3f0574a90ee66b
joshua <85379582+joshuaelliot@users.noreply.github.com>
2024-10-23 11:25:00 -0500

    Update app.js  usando cors

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const app = express();
 const albumsRouter = require('./routes/albums');
 const songsRouter = require('./routes/songs');
 const errorHandler = require('./middlewares/errorHandler');
+const cors = require("cors");
 const corsOptions = {
     origin: 'http://localhost:5500', // Origen permitido
     optionsSuccessStatus: 200
